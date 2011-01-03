2d77f354f72d3804c1455d2e23143c7537a0dbfd
Jeff Klein <jeffbk88@gmail.com>
2011-01-02 20:00:02 -0800

    added TidePrediction classes

--- app/controllers/tides_controller.rb
+++ app/controllers/tides_controller.rb
@@ -4,6 +4,83 @@ class Numeric
   end
 end
 
+class TideLevelDatum
+  attr_accessor :dt, :height
+  def intialize(dt, height)
+    @dt = dt
+    @height = height
+  end
+end
+
+class TidePrediction
+  attr_accessor :dt, :station_id, :datapoints
+  def initialize(dt, station_id)
+    @dt = dt
+    @station_id = station_id
+    @datapoints = []
+    @highs = []
+    @lows = []
+  end
+end
+    
+class Harmonic
+  def initialize(amp, speed, phase, nodefactor, equilibrium)
+    @amp = amp * nodefactor
+    @speed = speed
+    @phase = phase - equilibrium
+  end
+  
+  def contribution(t)
+    # time in hours
+    @amp * Math.cos((@speed * t / 60.0 - @phase).d_to_r)
+  end
+end
+
+class TidePredictor
+  DB_PATH = 'config/harmdb'
+  HARMONIC_DATA_SELECT = 'SELECT constituents.name, constituents.speed, constants.amp,
+                constants.phase, mod.node_factor, mod.equilibrium FROM data_sets d
+                JOIN constants ON constants.id = d.id
+                JOIN constituents ON constituents.name = constants.name
+                JOIN modulations mod ON mod.name = constants.name
+                WHERE d.id = ? AND mod.year = ?'
+  PREDICTION_LENGTH = 1440 # minutes
+  PREDICTION_STEP = 6 # minutes
+  def predict(station_id, dt)
+    tzero = Time.utc(dt.year, 1, 1, 0, 0)
+    tstart = (dt.to_i - tyear.to_i) / 60  # time in minutes
+    tend = tstart + PREDICTION_LENGTH
+    prediction = TidePrediction.new(dt, station_id, station_name)
+    harmonics = get_harmonics(station_id, dt.year)
+    (tstart..tend).step(PREDICTION_STEP) do |t|
+      height = 0 
+      harmonics.each do |harmonic|
+        height = height + harmonic.contribution(t / 60.0) # time in hours
+      end
+      prediction.datapoints << TideLevelDatum.new(Time.utc(tzero.to_i + t * 60), height)
+    end
+    return prediction
+  end  
+  
+  
+  def get_harmonics(id, year)
+    db = SQLite3::Database.new(DB_PATH)
+    rows = db.execute(HARMONIC_DATA_SELECT, station_id, year)
+    db.close()    
+    harmonics = []
+    rows.each do |row|
+      speed = Float(row[1])
+      amp = Float(row[2])
+      nf = Float(row[4])
+      phase = Float(row[3])
+      eq = Float(row[5]) 
+      harmonics << Harmonic.new(amp, speed, phase, nf, eq)
+    end
+    harmonics
+  end
+end
+  
+
 class TidesController < ApplicationController
   def predict
     tnow = Time.new()
@@ -23,7 +100,7 @@ class TidesController < ApplicationController
     tsec = tnow.to_i / 900 * 900 # truncate to nearest quarter hour
     t = (tsec - Time.utc(year, 1, 1, 0, 0).to_i) / 60.0  
     minutes = 1440
-    @heights = []
+    @datapoints = []
     @ticks = []
     @ymax = 0
     @ymin = 0
@@ -38,7 +115,7 @@ class TidesController < ApplicationController
         h = amp * nf * Math.cos((speed * ((t + i) / 60.0) + eq - phase).d_to_r)
         height = height + h
       end
-      @heights << [i, height]
+      @datapoints << [i, height]
       if i % 480 == 0 then
         @ticks << [i, Time.at(tsec + i * 60)]
       end
@@ -50,5 +127,4 @@ class TidesController < ApplicationController
       format.xml  { render :xml => @rows }
     end
   end
-
 end
